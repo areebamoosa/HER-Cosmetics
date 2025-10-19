4b840f04822b48af339c4a7169243d2f99ca78e7
Areeba Moosa <areebamoosa153@gmail.com>
2025-10-20 01:54:02 +0500

    Limited Addition Product

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Page from './Components/Hero-Page/Page'
 import Category from './Components/Category/Category'
+import Product from './Components/Limited-Addition/Product'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Page />
       <Category/>
+      <Product/>
     </>
   )
 }
---
+++ src/Components/Limited-Addition/Product.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import lip from '../../assets/virtualtry.webp'
+import Button from '../Button/Button'
+
+const Product = () => {
+  return (
+    <>
+      <div className='relative'>
+
+        <div className='w-full h-full object-cover'>
+
+          <img src={lip} alt="lip" className='w-full h-full object-cover' />
+        </div>
+
+        <div className='absolute bottom-1/2 left-1/2 -translate-x-1/2 text-white flex flex-col justify-center items-center '>
+
+
+          <h1 className=' dark-pink headFont text-3xl'>LIMITED EDITION</h1>
+          <div className='mt-4'>
+
+            <h2 className=' text-lg text-white font '>try on lipsticks, blushes & more to</h2>
+            <h2 className=' text-lg text-white font'>discover your new favourite shade.</h2>
+
+          </div>
+
+          <div className='mt-4'>
+
+            <Button text={'shop now'} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Product
