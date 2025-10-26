1bba4adcd8931604573ef9e9c0e469705e792233
Areeba Moosa <areebamoosa153@gmail.com>
2025-10-27 00:57:32 +0500

    Cosmetics Page Completed

--- src/Components/Cosmetics-Page/AllProducts.jsx
+++ src/Components/Cosmetics-Page/AllProducts.jsx
@@ -1,10 +1,37 @@
 import React, { useState } from 'react'
 import Product from '../Cosmetics-Page/Product'
-import main from '../../assets/prod.webp'
-import hovImg from '../../assets/hov.webp'
-import black from '../../assets/black.webp'
-import peach from '../../assets/peach.webp'
 import { Link } from 'react-router-dom'
+import main1 from '../../assets/pd1.webp'
+import hov1 from '../../assets/pdh1.webp'
+import main2 from '../../assets/pd2.webp'
+import hov2 from '../../assets/pdh2.webp'
+import main3 from '../../assets/pd3.webp'
+import hov3 from '../../assets/pd3h.webp'
+import s1 from '../../assets/pd3s-1.webp'
+import s2 from '../../assets/pd3s-2.webp'
+import s3 from '../../assets/pd3s-4.webp'
+import main4 from '../../assets/pd4.webp'
+import hov4 from '../../assets/pd4h.webp'
+import s4 from '../../assets/pd4s-1.webp'
+import main5 from '../../assets/pd5.webp'
+import hov5 from '../../assets/pd5h.webp'
+import s5 from '../../assets/pd5s-1.webp'
+import s6 from '../../assets/pd5s-2.webp'
+import s7 from '../../assets/pd5s-3.webp'
+import s8 from '../../assets/pd5s-4.webp'
+import main6 from '../../assets/pd6.webp'
+import hov6 from '../../assets/pd6h.webp'
+import main7 from '../../assets/pd7.webp'
+import hov7 from '../../assets/pd7h.webp'
+import s9 from '../../assets/pd7s-1.webp'
+import s10 from '../../assets/pd7s-2.webp'
+import s11 from '../../assets/pd7s-3.webp'
+import s12 from '../../assets/pd7s-4.webp'
+import main8 from '../../assets/pd8.webp'
+import hov8 from '../../assets/pd8h.webp'
+import s13 from '../../assets/pd8s-1.webp'
+import s14 from '../../assets/pd8s-2.webp'
+import s15 from '../../assets/pd8s-3.webp'
 
 export const productsDetails = [
   {
@@ -12,110 +39,109 @@ export const productsDetails = [
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main1,
+    hoverImg: hov1,
+    smallImgs: [hov1, main1]
   },
   {
     id: 2,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main2,
+    hoverImg: hov2,
+    smallImgs: [hov2, main2]
   },
   {
     id: 3,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main5,
+    hoverImg: hov5,
+    smallImgs: [s5, s6, s7, s8]
   },
   {
     id: 4,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main4,
+    hoverImg: hov4,
+    smallImgs: [main4, hov4, s4]
   },
   {
-    id: 1,
+    id: 5,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main3,
+    hoverImg: hov3,
+    smallImgs: [s1, s2, s3]
   },
   {
-    id: 2,
+    id: 6,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main6,
+    hoverImg: hov6,
+    smallImgs: [main6, hov6]
   },
   {
-    id: 3,
+    id: 7,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main7,
+    hoverImg: hov7,
+    smallImgs: [s9, s10, s11, s12]
   },
   {
-    id: 4,
+    id: 8,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main8,
+    hoverImg: hov8,
+    smallImgs: [s13, s14, s15]
   },
   {
-    id: 1,
+    id: 9,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main1,
+    hoverImg: hov1,
+    smallImgs: [hov1, main1]
   },
   {
-    id: 2,
+    id: 10,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main2,
+    hoverImg: hov2,
+    smallImgs: [hov2, main2]
   },
   {
-    id: 3,
+    id: 11,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main5,
+    hoverImg: hov5,
+    smallImgs: [s5, s6, s7, s8]
   },
   {
-    id: 4,
+    id: 12,
     title: "True Brown K",
     price: 36,
     desc: "King Kylie matte lip kit",
-    Img: main,
-    hoverImg: hovImg,
-    smallImgs: [black, peach, main, peach]
+    Img: main4,
+    hoverImg: hov4,
+    smallImgs: [main4, hov4, s4]
   }
-
 ]
 
 const AllProducts = () => {
@@ -124,8 +150,8 @@ const AllProducts = () => {
     <>
       <div className='grid grid-cols-4 gap-15 m-6'>
         {productsDetails.map((prod, index) => (
-          <Link to ={`/product/${prod.id}`} key={prod.id} >
-          <Product  {...prod} />
+          <Link to={`/product/${prod.id}`} key={prod.id} >
+            <Product  {...prod} />
           </Link>
         ))}
       </div>
