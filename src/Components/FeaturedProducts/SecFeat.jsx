1c107b5dff3b992769b698cecaed506547906d28
Areeba Moosa <areebamoosa153@gmail.com>
2025-11-01 22:54:29 +0500

    Fixed bug of main page's featured products

--- src/Components/FeaturedProducts/SecFeat.jsx
+++ src/Components/FeaturedProducts/SecFeat.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Product from '../Cosmetics-Page/Product'
 import { productsDetails as cosmetics } from '../Cosmetics-Page/AllProducts'
-import { productsDetails as skin } from '../Skin-Page/AllProducts'
-import { productsDetails as fragrance } from '../Fragrance-Page/AllProducts'
 
 const SecFeat = () => {
 
     const secFeatured = [
-        cosmetics[0], skin[3], cosmetics[5], skin[0]
+        cosmetics[0], cosmetics[1], cosmetics[2], cosmetics[3]
     ]
 
     return (
@@ -17,7 +15,7 @@ const SecFeat = () => {
                 <div className=' flex flex-col justify-center items-center mt-10'>
                     <div className='grid grid-cols-4 gap-8'>
                         {secFeatured.map((feat) => (
-                            <Link to={`/product /${feat.id}`} key={feat.id} >
+                            <Link to={`/product/${feat.id}`} key={feat.id} >
                                 <Product {...feat} />
                             </Link>
                         ))}
