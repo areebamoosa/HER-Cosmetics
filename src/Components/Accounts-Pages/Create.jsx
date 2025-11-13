f47d5de8b5442c6ea42dc3dad5424764e7e10b04
Areeba Moosa <areebamoosa153@gmail.com>
2025-11-14 01:29:01 +0500

    Login & Sign Up Page Responsive

--- src/Components/Accounts-Pages/Create.jsx
+++ src/Components/Accounts-Pages/Create.jsx
@@ -7,23 +7,21 @@ const Create = () => {
     return (
         <>
             <AccountNav />
-            <div className='flex flex-col justify-center items-center  mt-80 '>
+            <div className='flex flex-col justify-center items-center  mt-60 sm:mt-75 lg:mt-60 '>
 
-                <h1 className='headFont dark-pink text-3xl'>CREATE AN ACCOUNT</h1>
+                <h1 className='headFont dark-pink text-2xl sm:text-3xl'>CREATE AN ACCOUNT</h1>
 
                 <div className='flex gap-2'>
-                    <input type="text" placeholder='first name' className='w-60 h-10 bg-white grey p-6 rounded-sm mt-10 font' />
-                    <input type="text" placeholder='last name' className='w-60 h-10 bg-white grey p-6 rounded-sm mt-10 font' />
+                    <input type="text" placeholder='first name' className='w-48 sm:w-60 h-10 bg-white grey p-6 rounded-sm mt-10 font' />
+                    <input type="text" placeholder='last name' className='w-48 sm:w-60 h-10 bg-white grey p-6 rounded-sm mt-10 font' />
                 </div>
 
-                <input type="email" placeholder='email' className='w-120 h-10 bg-white grey p-6 rounded-sm mt-5 font' />
-                <input type="password" placeholder='password' className='w-120 h-10 bg-white grey p-6 rounded-sm mt-5 font' />
+                <input type="email" placeholder='email' className='w-100 sm:w-120 h-10 bg-white grey p-6 rounded-sm mt-5 font' />
+                <input type="password" placeholder='password' className='w-100 sm:w-120 h-10 bg-white grey p-6 rounded-sm mt-5 font' />
                 <div className="flex items-start text-left leading-tight mt-8 gap-2 justify-center">
 
-                    <input
-                        type="checkbox"
-                        className="  w-6 h-6 rounded-md border-2 border-black cursor-pointer " />
-                    <div className="text-sm flex gap-2">
+                    <input type="checkbox" className=" w-4 h-4 sm:w-6 sm:h-6 rounded-md border-2 border-black cursor-pointer " />
+                    <div className=" flex gap-1 text-xs sm:text-base">
                         <p className='grey'>
                             please agree to our{" "}
                             <span className="underline cursor-pointer">terms, privacy policy,</span>  to create an account
@@ -31,9 +29,9 @@ const Create = () => {
                     </div>
 
                 </div>
-                <button className='w-120 h-10  p-2 border-1 rounded-sm border-black mt-8 cursor-pointer grey'>create account</button>
-                <p className='mt-10 grey'>already have an account?</p>
-                <p className='cursor-pointer underline grey'> <Link to="/login">login</Link>   </p>
+                <button className='w-80 sm:w-120 h-10  p-2 border-1 rounded-sm border-black mt-8 cursor-pointer grey'>create account</button>
+                <p className='mt-10 grey text-sm sm:text-base'>already have an account?</p>
+                <p className='cursor-pointer underline grey text-sm sm:text-base mt-1'> <Link to="/login">login</Link>   </p>
             </div>
 
             <div className='mt-[290px]'>
--- src/Components/Accounts-Pages/Login.jsx
+++ src/Components/Accounts-Pages/Login.jsx
@@ -3,48 +3,48 @@ import AccountNav from '../Navbar/AccountNav'
 import Footer from '../Footer/Footer'
 import { Link } from 'react-router-dom'
 
-
 const Login = () => {
     return (
         <>
 
             <AccountNav />
 
-            <div className='flex flex-col justify-center items-center  mt-80 '>
+            <div className='flex flex-col justify-center items-center mt-60 sm:mt-75 lg:mt-60 '>
 
 
-                <h1 className='headFont dark-pink text-3xl'>LOGIN</h1>
+                <h1 className='headFont dark-pink text-2xl sm:text-3xl'>LOGIN</h1>
 
-                <input type="email" placeholder='email' className='w-120 h-10 bg-white grey p-6 rounded-sm mt-10 font' />
-                <input type="password" placeholder='password' className='w-120 h-10 bg-white grey p-6 rounded-sm mt-5 font' />
+                <input type="email" placeholder='email' className='w-100 sm:w-120 h-10 bg-white grey p-6 rounded-sm mt-10 font' />
+                <input type="password" placeholder='password' className='w-100 sm:w-120 h-10 bg-white grey p-6 rounded-sm mt-5 font' />
 
                 <div className='flex justify-end mt-2'>
-                    <p className='cursor-pointer underline ml-90 font grey'>forgot password?</p>
+                    <p className='cursor-pointer whitespace-nowrap underline ml-60 sm:ml-90 font grey text-sm sm:text-base'>forgot password?</p>
                 </div>
 
-                <div className='flex  text-left  leading-tight mt-8 gap-1 grey'>
+                <div className='flex text-sm sm:text-base text-left  leading-tight mt-8 gap-1 grey'>
+                    <input type="checkbox" />
                     <p>by logging in, you agree to our</p>
                     <p className='underline cursor-pointer'>terms, privacy policy,</p>
                     <p>and</p>
 
                 </div>
 
-                <div className='text-left grey'>
+                <div className='text-left grey text-sm sm:text-base'>
                     <p className='underline cursor-pointer'>rewards program terms</p>
 
                 </div>
 
 
-                <button className='w-120 h-10  p-2 border-1 rounded-sm border-black mt-8 cursor-pointer grey'>log in</button>
+                <button className='w-80 sm:w-120 h-10  p-2 border-1 rounded-sm border-black mt-8 cursor-pointer grey'>log in</button>
 
-                <p className='mt-10 grey'>don't have an account yet?</p>
-                <p className='cursor-pointer underline grey'><Link to="/create"> create account  </Link></p>
+                <p className='mt-10 grey text-sm sm:text-base'>don't have an account yet?</p>
+                <p className='cursor-pointer underline grey text-sm sm:text-base mt-2'><Link to="/create"> create account  </Link></p>
 
             </div>
 
-            <div className='mt-[290px]'>
-                <Footer />
-            </div>
+
+            <Footer />
+
         </>
     )
 }
