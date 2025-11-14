901840a0e37298972550520162660b9360e8f10f
Areeba Moosa <areebamoosa153@gmail.com>
2025-11-14 23:20:56 +0500

    Main Hero page responsive for all devices

--- src/Components/Hero-Page/Page.jsx
+++ src/Components/Hero-Page/Page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import main from '../../assets/main.png'
 import Button from '../Button/Button'
+import main from '../../assets/project.png'
 import Category from '../Category/Category'
 import Feat from '../FeaturedProducts/Feat'
 import Bag from '../Banners/Bag'
@@ -17,35 +17,61 @@ const Page = () => {
     <>
       <NavbarSwitcher />
 
+      {/* Hero Page Image */}
+
       <div>
-        <div className='relative h-screen w-screen'>
-
-          <img src={main} alt="hero" className='w-full h-full object-cover' />
-
-          <div className="absolute bottom-[200px] left-[23%] -translate-x-1/5  ">
-            <Button text="shop now" />
+        <div className="relative  h-[650px] sm:h-[600px] md:h-[1000px] lg:h-screen w-full">
+          <img src={main} alt="hero" className="w-full h-full object-cover object-[78%_20%] sm:object-[80%_20%]" />
+          <div className=" red sm:!text-white md:!text-white lg:!text-white absolute mt-4 sm:mt-0  translate-x-1/5  sm:bottom-[50px] sm:translate-x-1/5 lg:bottom-[120px] lg:left-[23%] lg:-translate-x-1/5">
+            <div className="flex items-center justify-center gap-4 mb-6">
+              <div className="h-[2px] w-20 red-bg sm:!bg-white"></div>
+              <p className="tracking-widest red text-base lg:text-4xl font-semibold sm:!text-white">
+                WINTER SALE
+              </p>
+              <div className="h-[2px] w-20 red-bg sm:!bg-white"></div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="text-5xl sm:text-7xl font-bold red sm:!text-white">
+                25% OFF
+              </h1>
+              <h2 className="text-3xl sm:text-5xl font-bold mt-4 red sm:!text-white">
+                SITEWIDE
+              </h2>
+              <div className="mt-5">
+                <Button text="shop now" />
+              </div>
+            </div>
           </div>
-
         </div>
       </div>
 
-      <Category />
+      <div className='mt-80 sm:mt-0'>
+        <Category />
+      </div>
+
       <Feat />
-      <div className='mt-20'>
+
+      <div className="mt-20">
         <Cosmic />
       </div>
+
       <Bag />
       <Blogger />
-      <div className='mt-20'>
+
+      <div className="mt-20">
         <Lipsticks />
       </div>
+
       <SecFeat />
-      <div className='mt-20'>
+
+      <div className="mt-20">
         <Limited />
       </div>
-      <div className='mt-100'>
+
+      <div className="mt-100">
         <Footer />
       </div>
+
     </>
   )
 }
